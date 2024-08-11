dd6eeb07d64823fdf98dd1846160550a8be4c253
cxj <1294520698@qq.com>
2024-08-11 18:03:13 +0800

    更新跨域问题

--- springboot/src/main/java/com/exam/controller/WebConfig.java
+++ springboot/src/main/java/com/exam/controller/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:8088") // 允许的前端端口
+                .allowedOrigins("*") // 允许所有来源
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的 HTTP 方法
                 .allowedHeaders("*"); // 允许的请求头
 
